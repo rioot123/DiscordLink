2fe42e591ba1d8757140d6b193c8fc8224a53b58
= <ShinyTourettesX@gmail.com>
2020-02-09 15:42:03 +0800

    Removed permission checks from method body of stop commands, already gets checked in the command builder.

--- src/main/java/net/dirtcraft/discord/discordlink/Commands/Discord/StopServer.java
+++ src/main/java/net/dirtcraft/discord/discordlink/Commands/Discord/StopServer.java
@@ -2,35 +2,25 @@ package net.dirtcraft.discord.discordlink.Commands.Discord;
 
 import net.dirtcraft.discord.discordlink.API.GuildMember;
 import net.dirtcraft.discord.discordlink.Commands.DiscordCommandExecutor;
-import net.dirtcraft.discord.discordlink.Configuration.PluginConfiguration;
 import net.dirtcraft.discord.discordlink.Utility;
-import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 
-import java.util.List;
-
 public class StopServer implements DiscordCommandExecutor {
-    private final Boolean gracefulExit;
+    private final boolean gracefulExit;
 
     public StopServer(boolean graceful){
         this.gracefulExit = graceful;
     }
-    public StopServer(){
-        this.gracefulExit = null;
-    }
 
     @Override
     public void execute(GuildMember source, String[] args, MessageReceivedEvent event) {
-        Role ownerRole = event.getGuild().getRoleById(PluginConfiguration.Roles.ownerRoleID);
-        Role dirtyRole = event.getGuild().getRoleById(PluginConfiguration.Roles.dirtyRoleID);
-        boolean gracefulExit = this.gracefulExit != null ? this.gracefulExit : args.length >= 2 && args[1].equals("-s");
-        List<Role> roles = event.getMember().getRoles();
-        if (roles.contains(ownerRole) || roles.contains(dirtyRole)) {
-            Utility.sendResponse(event, "Attempting to reboot the server.", 15);
-            FMLCommonHandler.instance().exitJava(-1, !gracefulExit);
-        } else {
-            Utility.sendPermissionErrorMessage(event);
+        Utility.sendResponse(event, "Attempting to reboot the server.", 15);
+        try{
+            Thread.sleep(50);
+        } catch (InterruptedException e){
+            e.printStackTrace();
         }
+        FMLCommonHandler.instance().exitJava(-1, !gracefulExit);
     }
 }
