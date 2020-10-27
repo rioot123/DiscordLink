af6c0fc3c7837fe00785604e1632437377cd09f1
Shiny <ShinyTourettesX@gmail.com>
2020-10-28 03:40:38 +0800

    fix sync

--- src/main/java/net/dirtcraft/discord/discordlink/Commands/Discord/IngameCommand.java
+++ src/main/java/net/dirtcraft/discord/discordlink/Commands/Discord/IngameCommand.java
@@ -1,6 +1,5 @@
 package net.dirtcraft.discord.discordlink.Commands.Discord;
 
-import net.dirtcraft.discord.discordlink.API.GuildMember;
 import net.dirtcraft.discord.discordlink.API.MessageSource;
 import net.dirtcraft.discord.discordlink.Commands.DiscordCommandExecutor;
 import net.dirtcraft.discord.discordlink.Commands.Sources.PrivateSender;
@@ -20,7 +19,7 @@ public class IngameCommand implements DiscordCommandExecutor {
     public void execute(MessageSource source, String command, List<String> args) {
         PrivateSender sender = new PrivateSender(source, command);
         Task.builder()
-                .execute( () -> Sponge.getCommandManager().process(sender, command))
+                .execute( () -> Sponge.getCommandManager().process(sender, this.command))
                 .submit(DiscordLink.getInstance());
     }
 }
